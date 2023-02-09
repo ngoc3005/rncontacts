b7dca6fb7f190ae222ec0fa62dec1770eb20359c
Ninh Van Chuong <chuongnv.nd@gmail.com>
2023-02-09 10:06:18 +0700

    get IP Address

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -1,14 +1,24 @@
 import React, {useContext, useEffect, useState} from 'react';
 import TimeKeepingComponent from '../../components/TimeKeeping';
 import {GlobalContext} from '../../context/Provider';
+import publicIP from 'react-native-public-ip';
 
 const TimeKeeping = () => {
+  const [form, setForm] = useState({});
   const {
     authDispath,
     authState: {error, loading},
   } = useContext(GlobalContext);
 
-  const onSubmit = () => {};
+  const onSubmit = () => {
+    publicIP()
+      .then(ip => {
+        form.ip = ip;
+      })
+      .catch(error => {
+        error = error;
+      });
+  };
   return (
     <TimeKeepingComponent onSubmit={onSubmit} error={error} loading={loading} />
   );
